e6a78c192281b8e5710eea2073cea22f3f7db2a0
Davide Marcato <davide.marcato@lnl.infn.it>
2021-01-19 17:02:33 +0100

    Using JSON data file

--- infn_jobs_bot.py
+++ infn_jobs_bot.py
@@ -7,12 +7,9 @@ import requests
 from bs4 import BeautifulSoup
 import telegram
 
-url = "https://jobs.dsi.infn.it/"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-}
-date_pattern = re.compile(r"(\d+-\d+-\d+)")
-users_json_file = "/data/users.json"
+url = "https://jobs.dsi.infn.it./data.php"
+data_folder = os.getenv("DATA_PATH", "/data/")
+users_json_file = os.getenv("USERS_JSON", data_folder + "users.json")
 check_period = int(os.getenv("CHECK_PERIOD", 600))
 token = os.getenv("TELEGRAM_TOKEN", None)
 
@@ -21,12 +18,12 @@ if token is None:
     exit(1)
 
 
-def download_webpage(url, headers):
+def download_webpage(url):
     try:
-        r = requests.get(url, headers=headers)
+        r = requests.get(url)
         r.raise_for_status()
 
-        webpage = r.text
+        data = r.json()
 
     except requests.exceptions.ConnectionError:
         print("Connection error")
@@ -44,36 +41,6 @@ def download_webpage(url, headers):
         print("Unknown request error")
         return None
 
-    return BeautifulSoup(webpage, "html.parser")
-
-
-def mine_data(soup):
-    data = {}
-
-    tables = soup.find_all("table")
-    for table in tables:
-        # Get table name
-        table_name = table.find_previous("h5").text.split(":")[0]
-
-        # List of column titles
-        table_heads = table.find("thead").find("tr").find_all("th")
-        heads = [th.text.strip() for th in table_heads]
-
-        items = []
-        table_body = table.find("tbody")
-        rows = table_body.find_all("tr")
-        for row in rows:
-            item = {}
-            cols = row.find_all("td")
-            for i, col in enumerate(cols):
-                links = []
-                for link in col.find_all("a"):
-                    links.append(link.extract())
-                text = col.text.strip()
-                item[heads[i]] = {"links": links, "text": text}
-
-            items.append(item)
-        data[table_name] = items
     return data
 
 
@@ -90,40 +57,37 @@ def save_data(filename, data):
     with open(filename, "wb") as f:
         pickle.dump(data, f)
 
-
-def compose_msg(offer, section_name):
-    msg = f"<b>{section_name}</b>\n"
-
-    title_text = offer["TITOLO"]["text"]
-    title_text = re.sub("\n{2,}", "\n", title_text)
-    title_text = re.sub(" {2,}", " ", title_text)
-    title_text = title_text.replace("\n ", "\n")
-    msg += title_text + "\n"
-
-    for link in offer["BANDO"]["links"]:
-        link_string = link.string
-        link_string = re.sub("\n", " ", link_string)
-        link_string = re.sub(" {2,}", " ", link_string)
-        link.string = link_string
-        msg += "\n" + str(link).strip()
-
-    msg += "\n"
-    if re.search(date_pattern, offer["BANDO"]["text"]):
-        msg += offer["BANDO"]["text"]
-    else:
-        msg += f"Scadenza: {offer['SCADENZA']['text']}"
-
+# offer = {
+#  unique_id: unique ID of job offers
+#  tipo: type of job
+#  numero: job offer number
+#  titolo: title
+#  scadenza: expiry date for application
+#  numero_posti*: number of available places
+#  durata*: job duration
+#  url_bando: URL to job offer notice
+# } *optional
+def compose_msg(offer):
+    msg = f"<b>{offer.get('tipo', 'OFFERTA DI LAVORO')}</b>\n"
+    msg += f"{offer.get('titolo', 'Titolo non disponibile')}\n"
+    
+    if 'numero_posti' in offer:
+        msg += f"Posti: {offer['numero_posti']}\n"
+    if 'durata' in offer:
+        msg += f"Durata: {offer['durata']}\n"
+    
+    msg += f"\nScadenza: {offer.get('scadenza', 'non disponibile')}\n"    
+    msg += f"<a href=\"{offer.get('url_bando', '#')}\">Scarica il bando {offer.get('numero', '')}</a>"
     return msg
 
 
 def find_new_offers(data, old_data):
-    new_offers = {}
-    for section_name, section in data.items():
-        new_offers[section_name] = []
-        for offer in section:
-            if offer not in old_data.get(section_name, []):
-                print("New offer")
-                new_offers[section_name].append(offer)
+    curr_ids = set([d["unique_id"] for d in data])
+    old_ids = set([d["unique_id"] for d in old_data])
+
+    new_ids = list(curr_ids - old_ids)
+    new_offers = [d for d in data if d["unique_id"] in new_ids]
+
     return new_offers
 
 
@@ -147,25 +111,22 @@ if __name__ == "__main__":
     bot = telegram.Bot(token=token)
 
     while True:
-        soup = download_webpage(url, headers)
-        if soup is None:
-            print("Could not download webpage")
+        data = download_webpage(url)
+        if data is None:
+            print("Could not download data")
             continue
 
-        data = mine_data(soup)
-        old_data = load_old_data("/data/data.pkl")
-        save_data("/data/data.pkl", data)
+        old_data = load_old_data(data_folder + "/data.pkl")
+        save_data(data_folder + "/data.pkl", data)
 
         if old_data is None:
             print("Could not load old data")
             continue
 
         new_offers = find_new_offers(data, old_data)
-        #new_offers["ASSEGNI DI RICERCA"].append(data["ASSEGNI DI RICERCA"][0])
-        for section_name, section in new_offers.items():
-            for offer in section:
-                msg = compose_msg(offer, section_name)
-                print(f"Sending offer to {len(users.values())} users:\n{msg}")
-                send_messages(users, msg)
+        for offer in new_offers:
+            msg = compose_msg(offer)
+            print(f"Sending offer to {len(users.values())} users:\n{msg}")
+            send_messages(users, msg)
 
         time.sleep(check_period)
